9a6a73b69ef9c20ba73742d4e8607829fdb596b9
Bruno Bucci <bruno@brunobucci.com>
2025-02-09 13:29:44 -0300

    Ajustes

--- init-mongo-usuario.js
+++ init-mongo-usuario.js
@@ -3,6 +3,7 @@ print("Iniciando script de inicialização do MongoDB...");
 // Criar a base Hackathon
 db = db.getSiblingDB("Hackathon");
 
+// Criar a collection usuario
 db.createCollection("usuario");
 print("Collection usuario criada!");
 
--- init-mongo-video.js
+++ init-mongo-video.js
@@ -3,13 +3,8 @@ print("Iniciando script de inicialização do MongoDB...");
 // Criar a base Hackathon
 db = db.getSiblingDB("Hackathon");
 
+// Criar a collection video
 db.createCollection("video");
 print("Collection video criada!");
-
-db.video.insertOne({
-    nome: "video.avi",
-    status: "Pendente"
-});
-print("Documento inicial inserido na collection video.");
 
 print("Finalizado script de inicialização do MongoDB.");

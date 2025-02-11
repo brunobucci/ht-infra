a5424ffe984fe3a8e0745d39ff7a1516d4210ff2
Bruno Bucci <bruno@brunobucci.com>
2025-02-10 22:02:20 -0300

    Update

---
+++ k8s/stress-test.sh
@@ -0,0 +1,240 @@
+#!/bin/bash
+for i in {1..10000}; do
+curl -X 'GET' \
+  'http://localhost:30081/swagger-ui/index.html' \
+  -d ''
+curl -X 'GET' \
+  'http://localhost:30081/swagger-ui/index.html' \
+  -d ''
+curl -X 'GET' \
+  'http://localhost:30081/swagger-ui/index.html' \
+  -d ''
+curl -X 'GET' \
+  'http://localhost:30081/swagger-ui/index.html' \
+  -d ''curl -X 'GET' \
+  'http://localhost:30081/swagger-ui/index.html' \
+  -d ''
+curl -X 'GET' \
+  'http://localhost:30081/swagger-ui/index.html' \
+  -d ''curl -X 'GET' \
+  'http://localhost:30081/swagger-ui/index.html' \
+  -d ''
+curl -X 'GET' \
+  'http://localhost:30081/swagger-ui/index.html' \
+  -d ''curl -X 'GET' \
+  'http://localhost:30081/swagger-ui/index.html' \
+  -d ''
+curl -X 'GET' \
+  'http://localhost:30081/swagger-ui/index.html' \
+  -d ''curl -X 'GET' \
+  'http://localhost:30081/swagger-ui/index.html' \
+  -d ''
+curl -X 'GET' \
+  'http://localhost:30081/swagger-ui/index.html' \
+  -d ''curl -X 'GET' \
+  'http://localhost:30081/swagger-ui/index.html' \
+  -d ''
+curl -X 'GET' \
+  'http://localhost:30081/swagger-ui/index.html' \
+  -d ''curl -X 'GET' \
+  'http://localhost:30081/swagger-ui/index.html' \
+  -d ''
+curl -X 'GET' \
+  'http://localhost:30081/swagger-ui/index.html' \
+  -d ''curl -X 'GET' \
+  'http://localhost:30081/swagger-ui/index.html' \
+  -d ''
+curl -X 'GET' \
+  'http://localhost:30081/swagger-ui/index.html' \
+  -d ''curl -X 'GET' \
+  'http://localhost:30081/swagger-ui/index.html' \
+  -d ''
+curl -X 'GET' \
+  'http://localhost:30081/swagger-ui/index.html' \
+  -d ''curl -X 'GET' \
+  'http://localhost:30081/swagger-ui/index.html' \
+  -d ''
+curl -X 'GET' \
+  'http://localhost:30081/swagger-ui/index.html' \
+  -d ''curl -X 'GET' \
+  'http://localhost:30081/swagger-ui/index.html' \
+  -d ''
+curl -X 'GET' \
+  'http://localhost:30081/swagger-ui/index.html' \
+  -d ''curl -X 'GET' \
+  'http://localhost:30081/swagger-ui/index.html' \
+  -d ''
+curl -X 'GET' \
+  'http://localhost:30081/swagger-ui/index.html' \
+  -d ''curl -X 'GET' \
+  'http://localhost:30081/swagger-ui/index.html' \
+  -d ''
+curl -X 'GET' \
+  'http://localhost:30081/swagger-ui/index.html' \
+  -d ''curl -X 'GET' \
+  'http://localhost:30081/swagger-ui/index.html' \
+  -d ''
+curl -X 'GET' \
+  'http://localhost:30081/swagger-ui/index.html' \
+  -d ''curl -X 'GET' \
+  'http://localhost:30081/swagger-ui/index.html' \
+  -d ''
+curl -X 'GET' \
+  'http://localhost:30081/swagger-ui/index.html' \
+  -d ''curl -X 'GET' \
+  'http://localhost:30081/swagger-ui/index.html' \
+  -d ''
+curl -X 'GET' \
+  'http://localhost:30081/swagger-ui/index.html' \
+  -d ''curl -X 'GET' \
+  'http://localhost:30081/swagger-ui/index.html' \
+  -d ''
+curl -X 'GET' \
+  'http://localhost:30081/swagger-ui/index.html' \
+  -d ''curl -X 'GET' \
+  'http://localhost:30081/swagger-ui/index.html' \
+  -d ''
+curl -X 'GET' \
+  'http://localhost:30081/swagger-ui/index.html' \
+  -d ''curl -X 'GET' \
+  'http://localhost:30081/swagger-ui/index.html' \
+  -d ''
+curl -X 'GET' \
+  'http://localhost:30081/swagger-ui/index.html' \
+  -d ''curl -X 'GET' \
+  'http://localhost:30081/swagger-ui/index.html' \
+  -d ''
+curl -X 'GET' \
+  'http://localhost:30081/swagger-ui/index.html' \
+  -d ''curl -X 'GET' \
+  'http://localhost:30081/swagger-ui/index.html' \
+  -d ''
+curl -X 'GET' \
+  'http://localhost:30081/swagger-ui/index.html' \
+  -d ''curl -X 'GET' \
+  'http://localhost:30081/swagger-ui/index.html' \
+  -d ''
+curl -X 'GET' \
+  'http://localhost:30081/swagger-ui/index.html' \
+  -d ''curl -X 'GET' \
+  'http://localhost:30081/swagger-ui/index.html' \
+  -d ''
+curl -X 'GET' \
+  'http://localhost:30081/swagger-ui/index.html' \
+  -d ''curl -X 'GET' \
+  'http://localhost:30081/swagger-ui/index.html' \
+  -d ''
+curl -X 'GET' \
+  'http://localhost:30081/swagger-ui/index.html' \
+  -d ''curl -X 'GET' \
+  'http://localhost:30081/swagger-ui/index.html' \
+  -d ''
+curl -X 'GET' \
+  'http://localhost:30081/swagger-ui/index.html' \
+  -d ''curl -X 'GET' \
+  'http://localhost:30081/swagger-ui/index.html' \
+  -d ''
+curl -X 'GET' \
+  'http://localhost:30081/swagger-ui/index.html' \
+  -d ''curl -X 'GET' \
+  'http://localhost:30081/swagger-ui/index.html' \
+  -d ''
+curl -X 'GET' \
+  'http://localhost:30081/swagger-ui/index.html' \
+  -d ''curl -X 'GET' \
+  'http://localhost:30081/swagger-ui/index.html' \
+  -d ''
+curl -X 'GET' \
+  'http://localhost:30081/swagger-ui/index.html' \
+  -d ''curl -X 'GET' \
+  'http://localhost:30081/swagger-ui/index.html' \
+  -d ''
+curl -X 'GET' \
+  'http://localhost:30081/swagger-ui/index.html' \
+  -d ''curl -X 'GET' \
+  'http://localhost:30081/swagger-ui/index.html' \
+  -d ''
+curl -X 'GET' \
+  'http://localhost:30081/swagger-ui/index.html' \
+  -d ''curl -X 'GET' \
+  'http://localhost:30081/swagger-ui/index.html' \
+  -d ''
+curl -X 'GET' \
+  'http://localhost:30081/swagger-ui/index.html' \
+  -d ''curl -X 'GET' \
+  'http://localhost:30081/swagger-ui/index.html' \
+  -d ''
+curl -X 'GET' \
+  'http://localhost:30081/swagger-ui/index.html' \
+  -d ''curl -X 'GET' \
+  'http://localhost:30081/swagger-ui/index.html' \
+  -d ''
+curl -X 'GET' \
+  'http://localhost:30081/swagger-ui/index.html' \
+  -d ''curl -X 'GET' \
+  'http://localhost:30081/swagger-ui/index.html' \
+  -d ''
+curl -X 'GET' \
+  'http://localhost:30081/swagger-ui/index.html' \
+  -d ''curl -X 'GET' \
+  'http://localhost:30081/swagger-ui/index.html' \
+  -d ''
+curl -X 'GET' \
+  'http://localhost:30081/swagger-ui/index.html' \
+  -d ''curl -X 'GET' \
+  'http://localhost:30081/swagger-ui/index.html' \
+  -d ''
+curl -X 'GET' \
+  'http://localhost:30081/swagger-ui/index.html' \
+  -d ''curl -X 'GET' \
+  'http://localhost:30081/swagger-ui/index.html' \
+  -d ''
+curl -X 'GET' \
+  'http://localhost:30081/swagger-ui/index.html' \
+  -d ''curl -X 'GET' \
+  'http://localhost:30081/swagger-ui/index.html' \
+  -d ''
+curl -X 'GET' \
+  'http://localhost:30081/swagger-ui/index.html' \
+  -d ''curl -X 'GET' \
+  'http://localhost:30081/swagger-ui/index.html' \
+  -d ''
+curl -X 'GET' \
+  'http://localhost:30081/swagger-ui/index.html' \
+  -d ''curl -X 'GET' \
+  'http://localhost:30081/swagger-ui/index.html' \
+  -d ''
+curl -X 'GET' \
+  'http://localhost:30081/swagger-ui/index.html' \
+  -d ''curl -X 'GET' \
+  'http://localhost:30081/swagger-ui/index.html' \
+  -d ''
+curl -X 'GET' \
+  'http://localhost:30081/swagger-ui/index.html' \
+  -d ''curl -X 'GET' \
+  'http://localhost:30081/swagger-ui/index.html' \
+  -d ''
+curl -X 'GET' \
+  'http://localhost:30081/swagger-ui/index.html' \
+  -d ''curl -X 'GET' \
+  'http://localhost:30081/swagger-ui/index.html' \
+  -d ''
+curl -X 'GET' \
+  'http://localhost:30081/swagger-ui/index.html' \
+  -d ''curl -X 'GET' \
+  'http://localhost:30081/swagger-ui/index.html' \
+  -d ''
+curl -X 'GET' \
+  'http://localhost:30081/swagger-ui/index.html' \
+  -d ''curl -X 'GET' \
+  'http://localhost:30081/swagger-ui/index.html' \
+  -d ''
+curl -X 'GET' \
+  'http://localhost:30081/swagger-ui/index.html' \
+  -d ''curl -X 'GET' \
+  'http://localhost:30081/swagger-ui/index.html' \
+  -d ''
+curl -X 'GET' \
+  'http://localhost:30081/swagger-ui/index.html' \
+  -d ''
+done
